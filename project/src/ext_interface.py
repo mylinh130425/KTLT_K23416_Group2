c1ca72792c818db8b962ac0ebfcc381289b98df6
arishuynhvan <arishuynhvan@gmail.com>
2025-03-19 13:32:47 +0700

    Set up automatic generation of dóc

--- project/src/ext_interface.py
+++ project/src/ext_interface.py
@@ -4,10 +4,31 @@ from project.src.ui_interface_stacked import *
 
 class Extend_MainWindow(Ui_MainWindow):
     def setupUi(self, MainWindow):
+        """
+        Set up the main window UI by initializing the user interface components
+        and connecting signals to their respective slots.
+
+        Args:
+            MainWindow (QMainWindow): The main window to set up the UI for.
+        """
         super().setupUi(MainWindow)
         self.processSignalAndSlot()
 
+
     def processSignalAndSlot(self):
+        """
+        Connects UI buttons to their respective slot functions to handle user interactions.
+
+        This method establishes connections between the click events of the main
+        navigation buttons and their corresponding slot methods. Each button, when
+        clicked, triggers its associated function to navigate within the application
+        or perform specific actions.
+        """
+        self.login_button.clicked.connect(self.loginClicked)
+        self.to_login_button.clicked.connect(self.goLogin)
+        self.to_signup_button.clicked.connect(self.goSignUp)
+        self.home_button.clicked.connect(self.goHome)
+        self.restaurant_button.clicked.connect(self.goRestaurant)
         self.login_button.clicked.connect(self.loginClicked)
         self.to_login_button.clicked.connect(self.goLogin)
         self.to_signup_button.clicked.connect(self.goSignUp)
@@ -15,19 +36,50 @@ class Extend_MainWindow(Ui_MainWindow):
         self.restaurant_button.clicked.connect(self.goRestaurant)
 
     def goLogin(self):
+        """
+        Sets the login page as the current widget of the
+        `login_signup_stackedWidget`.
+        """
         self.login_signup_stackedWidget.setCurrentWidget(self.right_login_page)
+
     def goSignUp(self):
+        """
+        Sets the signup page as the current widget of the
+        `login_signup_stackedWidget`.
+        """
         self.login_signup_stackedWidget.setCurrentWidget(self.right_signup_page)
 
     def goHome(self):
+        """
+        Sets the home page as the current widget of the
+        `body_stackedWidget` and removes all other widgets except the home page.
+        """
         self.body_stackedWidget.setCurrentWidget(self.home_page)
         self.removeAllWidgetsExcept(self.body_stackedWidget,self.home_page)
 
     def goRestaurant(self):
+        """
+        Sets the restaurant page as the current widget of the
+        `body_stackedWidget` and removes all other widgets except the restaurant page.
+        """
+        self.body_stackedWidget.setCurrentWidget(self.restaurant_page)
+        self.removeAllWidgetsExcept(self.body_stackedWidget, self.restaurant_page)
         self.body_stackedWidget.setCurrentWidget(self.restaurant_page)
         self.removeAllWidgetsExcept(self.body_stackedWidget,self.restaurant_page)
 
-    def removeAllWidgetsExcept(self, stack, w):
+    def removeAllWidgetsExcept(self, stack: QtWidgets.QStackedWidget, w: QtWidgets.QWidget):
+        """
+        Removes all widgets from the given stack except the one specified.
+
+        This method goes through all the widgets in the stack and removes each one
+        that is not the widget specified by the `w` argument. This is done by
+        iterating over the widgets in reverse order, calling `removeWidget` and
+        `deleteLater` on each one.
+
+        Args:
+            stack (QtWidgets.QStackedWidget): The stack to remove widgets from.
+            w (QtWidgets.QWidget): The only widget to keep in the stack.
+        """
         for i in reversed(range(stack.count())):
             widget = stack.widget(i)
             if widget is not w:  # Chỉ xóa nếu KHÔNG phải w
@@ -36,14 +88,30 @@ class Extend_MainWindow(Ui_MainWindow):
                 widget.deleteLater()
 
     def setup_profile(self):
+        """
+        Initializes the profile page and adds it to the body stacked widget.
+
+        This method creates an instance of the ProfileScreen and adds it to the
+        `body_stackedWidget`. This allows the profile page to be displayed within
+        the main application window when navigating to the profile section.
+        """
         self.profile_page = ProfileScreen(self.body_stackedWidget)
         self.body_stackedWidget.addWidget(self.profile_page)  # Thêm trực tiếp
 
 
     def loginClicked(self):
+        """
+        Handles the login button click event.
+
+        This method is triggered when the login button is clicked. It sets up
+        the profile page, adds it to the body stacked widget, and updates the
+        current widget of the stacked widget and body stacked widget to display
+        the main and profile page, respectively.
+        """
         print("setting up profile page")
         self.setup_profile()
         self.stackedWidget.setCurrentWidget(self.Main)
         self.body_stackedWidget.setCurrentWidget(self.profile_page)
+
 
 
