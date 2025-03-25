2a6dc50bf885b75d158efb7c6664343d80887b65
arishuynhvan <arishuynhvan@gmail.com>
2025-03-25 18:18:46 +0700

    Revert to previous commit and remove unwanted file

--- project/src/view/ClickableLabel.py
+++ project/src/view/ClickableLabel.py
@@ -1,5 +1,6 @@
+from PIL.ImageQt import QPixmap
 from PyQt6.QtCore import pyqtSignal
-from PyQt6.QtWidgets import QLabel
+from PyQt6.QtWidgets import QLabel, QFileDialog
 
 
 class ClickableLabel(QLabel):
@@ -10,3 +11,17 @@ class ClickableLabel(QLabel):
 
     def mousePressEvent(self, event):
         self.clicked.emit()  # Emit signal when clicked
+    def open_file_dialog(self, ):
+        file_dialog = QFileDialog()
+        file_path, _ = file_dialog.getOpenFileName(
+            None, "Select an Image", "", "Images (*.png *.jpg *.jpeg *.bmp *.gif)"
+        )
+
+        if file_path:  # If a file is selected
+            self.file_path = file_path  # Save the path
+            self.setPixmap(QPixmap(file_path).scaled(
+                self.width(),
+                self.height()
+            ))  # Load image to QLabel instantly
+        print(self.restaurant_new_image_path)
+        return file_path
