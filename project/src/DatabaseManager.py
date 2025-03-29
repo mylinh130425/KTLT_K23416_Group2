a0253c05f146222910b2b0db46c696b2cf047d3e
= <=>
2025-03-29 16:40:20 +0700

    Hiện MenuPage nhưng bị lỗi nút btn ở ResScreen

--- project/src/DatabaseManager.py
+++ project/src/DatabaseManager.py
@@ -36,18 +36,9 @@ class DatabaseManager:
         self.db = self.client[db_name]
         self.users = self.db["Users"]
         self.restaurants=self.db["Restaurants"]
-        self.menu = self.db["Menu"]
         # Đảm bảo username và email là duy nhất
         self.users.create_index("username", unique=True)
         # self.users.create_index("email", unique=True)
-
-    def get_review_list_by_restaurant_id(self, restaurant_id):
-        return self.restaurants.find_one({"_id": ObjectId(restaurant_id)})["detailed_reviews"]
-    def get_review_list_by_food(self, product_id):
-        return self.menu.find_one(
-            {"menu.product_id": product_id},
-            {"menu.$": 1, "_id": 0}  # Exclude _id if not needed
-        )["menu"][0]["item_review"]
 
     def add_restaurant_to_db(self, restaurant_data: dict) -> bool:
         try:
@@ -149,55 +140,6 @@ class DatabaseManager:
         except Exception as e:
             print(f"DatabaseManager: Error in get_menu_by_place_id: {e}")
             return []
-    # def get_review_by_place_id(self, place_id, offset=0, limit=15):
-    #     print(f"DB received place_id: {place_id}")
-    #     if self.db is None:
-    #         print("DatabaseManager: Cannot fetch reviews - MongoDB connection failed.")
-    #         return []
-    #
-    #     try:
-    #         collection = self.db["Restaurants"]
-    #         print(f"Type of place_id: {type(place_id)}")
-    #         print(f"Total documents in Menu collection: {collection.count_documents({})}")
-    #
-    #         # Chuyển place_id từ chuỗi sang ObjectId
-    #         place_id_obj = ObjectId(place_id)
-    #         print(f"Converted place_id to ObjectId: {place_id_obj}")
-    #
-    #         # Tìm tài liệu có place_id khớp với _id của nhà hàng
-    #         restaurant_reviews = collection.find_one({"place_id": place_id_obj})
-    #         print(f"Raw menu data: {restaurant_reviews}")
-    #
-    #         if not restaurant_reviews:
-    #             print(f"DatabaseManager: No review found for place_id {place_id}.")
-    #             all_restaurant_reviews = collection.find({"place_id": place_id_obj})
-    #             print(f"Debug: All documents with place_id {place_id}: {list(all_restaurant_reviews)}")
-    #             all_place_ids = collection.distinct("place_id")
-    #             print(f"All place_ids in Restaurant collection: {all_place_ids}")
-    #             return []
-    #
-    #         review_list_data = restaurant_reviews.get("detailed_reviews", [])
-    #         if not review_list_data:
-    #             print(f"DatabaseManager: No reviews found for place_id {place_id}.")
-    #             return []
-    #
-    #         if limit is not None:
-    #             review_list_data = review_list_data[offset:offset + limit]
-    #         else:
-    #             review_list_data = review_list_data[offset:]
-    #
-    #         review_list = []
-    #         for item in review_list_data:
-    #             review_entry = {}
-    #             for key, value in item.items():
-    #                 setattr(review_entry, key, value)
-    #             review_list.append(review_entry)
-    #         print(f"{len(review_list)} items returned from db")
-    #         print(review_list[0])
-    #         return review_list
-    #     except Exception as e:
-    #         print(f"DatabaseManager: Error in get_menu_by_place_id: {e}")
-    #         return []
 
     def get_all_menus(self, offset=0, limit=15):
         if self.db is None:
@@ -335,50 +277,25 @@ class DatabaseManager:
             if not restaurant_data:
                 return None
 
-            # return {
-            #     "featured_image": restaurant_data.get("featured_image", ""),
-            #     "name": restaurant_data.get("name", "Unknown"),
-            #     "category": restaurant_data.get("categories", []),
-            #     "address": {
-            #         "city": restaurant_data.get("detailed_address", {}).get("city", ""),
-            #         "state": restaurant_data.get("detailed_address", {}).get("state", ""),
-            #         "ward": restaurant_data.get("detailed_address", {}).get("ward", ""),
-            #         "street": restaurant_data.get("detailed_address", {}).get("street", "")
-            #     },
-            #     "detailed_address": restaurant_data.get("address", ""),
-            #     "phone": restaurant_data.get("phone", "N/A"),
-            #     "website": restaurant_data.get("website", "N/A"),
-            #     "hours": restaurant_data.get("hours", []),
-            #     "about": restaurant_data.get("about", [])
-            # }
-            restaurant_data
+            return {
+                "featured_image": restaurant_data.get("featured_image", ""),
+                "name": restaurant_data.get("name", "Unknown"),
+                "category": restaurant_data.get("categories", []),
+                "address": {
+                    "city": restaurant_data.get("detailed_address", {}).get("city", ""),
+                    "state": restaurant_data.get("detailed_address", {}).get("state", ""),
+                    "ward": restaurant_data.get("detailed_address", {}).get("ward", ""),
+                    "street": restaurant_data.get("detailed_address", {}).get("street", "")
+                },
+                "detailed_address": restaurant_data.get("address", ""),
+                "phone": restaurant_data.get("phone", "N/A"),
+                "website": restaurant_data.get("website", "N/A"),
+                "hours": restaurant_data.get("hours", []),
+                "about": restaurant_data.get("about", [])
+            }
         except Exception as e:
             print(f"Error fetching restaurant: {e}")
             return None
-    def add_review_to_menu(self, review, product_id):
-        try:
-            result = self.restaurants.update_one(
-                {
-                    # "place_id": review["place_id"],
-                    "menu.product_id": product_id
-                },
-                {
-                    "$push": {
-                        "menu.$.item_review": review["review"]
-                    }
-                }
-            )
-
-            if result.modified_count > 0:
-                print("Review added successfully.")
-                return True
-            else:
-                print("No matching food item found.")
-                return False
-
-        except Exception as e:
-            print(f"Error adding review: {e}")
-            return False
 
 
 if __name__ == "__main__":
@@ -411,10 +328,3 @@ if __name__ == "__main__":
         print("Restaurant data:", restaurant_data)
     else:
         print("No restaurant data found!")
-
-    review_data = db_manager.get_review_list_by_food("HC0001")
-    if review_data:
-        print("Review data:", review_data)
-    else:
-        print("No review data found!")
-
